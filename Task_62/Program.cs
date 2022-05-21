953b6566db10c3968d0ec10a99247678b29fa6cf
Vetrov Yury <888vetrov@gmail.com>
2022-05-21 23:42:30 +0300

    TaSk_62+

--- Task_62/Program.cs
+++ Task_62/Program.cs
@@ -4,121 +4,56 @@
 //11 16 15 6
 //10 9 8 7
 
-using System;
-using System.Collections.Generic;
- 
-class GFG {
- 
-    public static int R = 4, C = 4;
-    public static bool isInBounds(int i, int j)
-    {
-        if (i < 0 || i >= R || j < 0 || j >= C)
-            return false;
- 
-        return true;
+void spiralFill(int m, int n, int[,] array) //Создадим функцию для заполнения массива спиралью
+{
+		int value = 1; // Объявляем значение для заполнения матрицы
+            /*
+            k - начальный индекс стрoки
+            m - конечный индекс строки
+            l - начальный индекс столбца
+            n - конечный индекс столбца
+            */
+		int k = 0, l = 0;
+		while (k < m && l < n) 
+        {
+			// Найдём первую строку из оставшихся строк
+			for (int i = l; i < n; ++i) 
+            {
+				array[k, i] = value++;
+			}
+			k++;
+			// Найдём последний столбец из оставшихся столбцов 
+			for (int i = k; i < m; ++i) 
+            {
+				array[i,n - 1] = value++;
+			}
+			n--;
+			// Найдём последнюю строку из оставшихся рядов
+			if (k < m) 
+            {
+				for (int i = n - 1; i >= l; --i) 
+                {
+					array[m - 1, i] = value++;
+				}
+				m--;
+			}
+			// Найдём первый столбец из оставшихся столбцов
+			if (l < n) 
+            {
+				for (int i = m - 1; i >= k; --i) 
+                {
+					array[i,l] = value++;
+				}
+				l++;
+			}
+		}
     }
- 
-    // Check if the position is blocked
-    public static bool isBlocked(int[, ] matrix, int i,
-                                 int j)
+int m = 4, n = 4;
+int[,] array = new int[m, n];
+spiralFill(m, n, array);
+for (int i = 0; i < m; i++) 
     {
-        if (!isInBounds(i, j))
-            return true;
-        if (matrix[i, j] == -1)
-            return true;
-        return false;
-    }
- 
-    // DFS code to traverse spirally
-    public static void spirallyDFSTravserse(int[, ] matrix,
-                                            int i, int j,
-                                            int dir,
-                                            List<int> res)
-    {
-        if (isBlocked(matrix, i, j))
-            return;
- 
-        bool allBlocked = true;
- 
-        for (int k = -1; k <= 1; k += 2) {
-            allBlocked = allBlocked
-                         && isBlocked(matrix, k + i, j)
-                         && isBlocked(matrix, i, j + k);
-        }
-        res.Add(matrix[i, j]);
-        matrix[i, j] = -1;
- 
-        if (allBlocked) {
-            return;
-        }
- 
-        // dir: 0 - right, 1 - down, 2 - left, 3 - up
-        int nxt_i = i;
-        int nxt_j = j;
-        int nxt_dir = dir;
-        if (dir == 0) {
-            if (!isBlocked(matrix, i, j + 1)) {
-                nxt_j++;
-            }
-            else {
-                nxt_dir = 1;
-                nxt_i++;
-            }
-        }
-        else if (dir == 1) {
-            if (!isBlocked(matrix, i + 1, j)) {
-                nxt_i++;
-            }
-            else {
-                nxt_dir = 2;
-                nxt_j--;
-            }
-        }
-        else if (dir == 2) {
-            if (!isBlocked(matrix, i, j - 1)) {
-                nxt_j--;
-            }
-            else {
-                nxt_dir = 3;
-                nxt_i--;
-            }
-        }
-        else if (dir == 3) {
-            if (!isBlocked(matrix, i - 1, j)) {
-                nxt_i--;
-            }
-            else {
-                nxt_dir = 0;
-                nxt_j++;
-            }
-        }
-        spirallyDFSTravserse(matrix, nxt_i, nxt_j, nxt_dir,
-                             res);
-    }
- 
-    // To traverse spirally
-    public static List<int> spirallyTraverse(int[, ] matrix)
-    {
-        List<int> res = new List<int>();
-        spirallyDFSTravserse(matrix, 0, 0, 0, res);
-        return res;
-    }
- 
-    // Driver code
-    static public void Main()
-    {
-        int[, ] a = { { 1, 2, 3, 4 },
-                      { 5, 6, 7, 8 },
-                      { 9, 10, 11, 12 },
-                      { 13, 14, 15, 16 } };
- 
-        // Function Call
-        List<int> res = spirallyTraverse(a);
-        int size = res.Count;
- 
-        for (int i = 0; i < size; ++i)
-            Console.Write(res[i] + " ");
- 
-        Console.WriteLine();
-    }
-}
+		for (int j = 0; j < n; j++) 
+		Console.Write($"{array[i, j], 4}");
+		Console.Write("\n");
+	}
